bde50027ef707564fd4c04560744befe3936792f
aseem <shrestha.asm@gmail.com>
2022-02-06 13:30:11 -0500

    move refresh token to token controller

--- web/src/main/java/com/babydocs/controller/UserController.java
+++ web/src/main/java/com/babydocs/controller/UserController.java
@@ -1,16 +1,11 @@
 package com.babydocs.controller;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.babydocs.constants.ApiConstants;
 import com.babydocs.constants.RoleBuilder;
 import com.babydocs.constants.UserStatus;
 import com.babydocs.exceptions.ResourceNotFoundException;
 import com.babydocs.logger.AppLogger;
 import com.babydocs.model.User;
-import com.babydocs.security.JwtProperties;
 import com.babydocs.service.AppService;
 import com.babydocs.service.UserValidationService;
 import eu.bitwalker.useragentutils.UserAgent;
@@ -20,13 +15,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.io.IOException;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -93,36 +84,4 @@ public class UserController
                            + user.getEmail());
         return new ResponseEntity<>(userToSave, HttpStatus.CREATED);
     }
-
-    @GetMapping( "v1/public/token-refresh" )
-    public ResponseEntity<?> tokenRefresh(HttpServletRequest request, HttpServletResponse response) throws IOException
-    {
-        String tokenHeader = request.getHeader(JwtProperties.REFRESH_TOKEN);
-        String _token = tokenHeader.substring("Bearer ".length());
-        Algorithm algorithm = Algorithm.HMAC512(JwtProperties.SECRET.getBytes());
-        JWTVerifier verifier = JWT.require(algorithm).build();
-        DecodedJWT decodedJWT = verifier.verify(_token);
-        String username = decodedJWT.getSubject();
-        Optional<User> user = appService.getUser(username);
-        String role = "";
-        if (user.isPresent()) {
-            User _user = user.get();
-            role = _user.getRole().getRole();
-        }
-
-        final String auth_token = JWT.create()
-            .withSubject(username)
-            .withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.ACCESS_TOKEN_EXPIRATION_TIME))
-            .withIssuer(request.getRequestURL().toString())
-            .withClaim("role", "ROLE_" + role)
-            .sign(Algorithm.HMAC512(JwtProperties.SECRET.getBytes()));
-
-        AppLogger.info(UserController.class, "Auth Token created: " + auth_token);
-        Map<String, String> tokenMap = new HashMap<>();
-        tokenMap.put("access_token", JwtProperties.TOKEN_PREFIX + auth_token);
-        tokenMap.put("refresh_token", tokenHeader);
-        return new ResponseEntity<>(tokenMap, HttpStatus.OK);
-
-    }
-
 }
