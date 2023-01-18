fd15fa4f8dc0898cb313cf0957da21823df6b39e
Aseem <aseem@Aseems-MacBook-Pro.local>
2023-01-17 23:29:09 -0500

    bugfix

--- domain/src/main/java/com/babydocs/model/Baby.java
+++ domain/src/main/java/com/babydocs/model/Baby.java
@@ -4,9 +4,20 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -19,17 +30,18 @@ public class Baby implements Serializable {
     @JsonIgnore
     private Long id;
 
-    @NotEmpty(message = "First Name is required")
+    @NotNull(message = "First Name is required")
     @Column(nullable = false)
     private String firstName;
 
-    @NotEmpty(message = "Last Name is required")
+    @NotNull(message = "Last Name is required")
     @Column(nullable = false)
     private String lastName;
 
     @Temporal(TemporalType.DATE)
     @Column(name = "dob", nullable = false)
-    @NotEmpty(message = "Dob is required")
+    @NotNull(message = "Dob is required")
+    @DateTimeFormat(pattern = "MM-dd-yyyy")
     private Date dob;
 
     private String gender;
@@ -38,12 +50,14 @@ public class Baby implements Serializable {
     @Column(name = "created", nullable = false, updatable = false)
     @CreatedDate
     @JsonIgnore
+    @DateTimeFormat(pattern = "MM-dd-yyyy")
     private Date created;
 
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "lastUpdated", nullable = false)
     @LastModifiedDate
     @JsonIgnore
+    @DateTimeFormat(pattern = "MM-dd-yyyy")
     private Date lastUpdated;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
